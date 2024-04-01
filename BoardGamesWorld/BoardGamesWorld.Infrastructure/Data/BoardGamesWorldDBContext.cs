d62264f10a731da42fbffa70df67aff779c8a8a9
AAngelov00 <atanasangelov53@gmail.com>
2024-04-01 10:54:18 +0300

    Extensions from Prgoram.cs

---
+++ BoardGamesWorld/BoardGamesWorld.Infrastructure/Data/BoardGamesWorldDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoardGamesWorld.Infrastructure.Data
+{
+    public class BoardGamesWorldDBContext : IdentityDbContext
+    {
+        protected BoardGamesWorldDBContext(DbContextOptions<BoardGamesWorldDBContext> options) :
+            base(options)
+        {
+        }
+    }
+}
---
+++ BoardGamesWorld/BoardGamesWorld/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,44 @@
+﻿using BoardGamesWorld.Data;
+using BoardGamesWorld.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDBContext(this IServiceCollection service, IConfiguration config)
+        {
+
+            var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            service.AddDbContext<BoardGamesWorldDBContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            service.AddDatabaseDeveloperPageExceptionFilter();
+
+            return service;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection service, IConfiguration config)
+        {
+            service
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit= false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            return service;
+
+        }
+
+    }
+}

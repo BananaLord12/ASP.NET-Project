a96622c0c9aa516c8caed5d4e80091d51ef74843
AAngelov00 <atanasangelov53@gmail.com>
2024-04-03 11:16:19 +0300

    Implemented the Repositoy and some Services

---
+++ BoardGamesWorld/BoardGamesWorld.Core/Costants/IBoardGameService.cs
@@ -0,0 +1,18 @@
+﻿using BoardGamesWorld.Core.Models.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamesWorld.Core.Costants
+{
+    public interface IBoardGameService
+    {
+        Task<IEnumerable<BoardGamesIndexServiceModel>> LastThreeBoardGamesAsync();
+
+        Task<IEnumerable<BoardGamesAllServiceModel>> AllBoardGamesAsync();
+
+
+    }
+}
---
+++ BoardGamesWorld/BoardGamesWorld.Core/Services/BoardGamesService.cs
@@ -0,0 +1,47 @@
+﻿using BoardGamesWorld.Core.Costants;
+using BoardGamesWorld.Core.Models.Home;
+using BoardGamesWorld.Infrastructure.Data.Common;
+using BoardGamesWorld.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoardGamesWorld.Core.Services
+{
+    public class BoardGamesService : IBoardGameService
+    {
+        private readonly IRepository repository;
+
+        public BoardGamesService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<IEnumerable<BoardGamesAllServiceModel>> AllBoardGamesAsync()
+        {
+            return await repository
+                    .AllReadOnly<BoardGame>()
+                    .OrderBy(b => b.Id)
+                    .Select(b => new BoardGamesAllServiceModel()
+                    {
+                        Id = b.Id,
+                        ImageUrl = b.ImageUrl,
+                        Name = b.Name
+                    })
+                    .ToListAsync();
+        }
+
+        public async Task<IEnumerable<BoardGamesIndexServiceModel>> LastThreeBoardGamesAsync()
+        {
+            return await repository
+                .AllReadOnly<BoardGame>()
+                .OrderByDescending(b => b.Id)
+                .Take(3)
+                .Select(b => new BoardGamesIndexServiceModel()
+                {
+                    Id = b.Id,
+                    ImageUrl = b.ImageUrl,
+                    Name = b.Name
+                })
+                .ToListAsync();
+        }
+    }
+}
